847a68783ba28bec898f70922027639ea8109d98
Danil Ponomarenko <el.pochtadp@gmail.com>
2026-03-03 14:11:05 +0300

    Update CustomsService.java

--- ru/netology/service/CustomsService.java
+++ ru/netology/service/CustomsService.java
@@ -1,10 +1,9 @@
 package ru.netology.service;
 
 public class CustomsService {
+    public static final int ST_PO_WEIGHT = 100;
 
     public static int calculateCustoms(int price, int weight) {
-        final int ST_PO_WEIGHT = 100;
-
         int result = (price / 100) + (weight * ST_PO_WEIGHT);
         return result;
     }
